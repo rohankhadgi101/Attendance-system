96625e51a4a5f288dd0518a9f5e54f1602b1d361
Hate Me Enough <neet.khadgee@gmail.com>
2023-07-02 08:57:06 +0545

    loginpage

---
+++ app/src/main/java/com/attendanceapp/Endpoints.java
@@ -0,0 +1,14 @@
+package com.attendanceapp;
+
+public class Endpoints {
+    public class Endpts {
+
+        private static final String base_url = "http://localhost/attendanceapp/";
+
+        public static final String login_url = base_url + "login.php";
+
+
+    }
+
+
+}
