b1d206c8d2a23d212ce677c7bf1a78f5e610342b
Hate Me Enough <neet.khadgee@gmail.com>
2023-07-06 17:13:09 +0545

    loginpage

--- app/src/main/java/com/attendanceapp/MainActivity.java
+++ app/src/main/java/com/attendanceapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.attendanceapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,15 +17,21 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private EditText usernameEditText, passwordEditText;
     private Button loginButton;
+
     private RequestQueue requestQueue;
-    //  private static final String API_URL = "http://demo1909133.mockable.io/login";
-    private static final String API_URL = "http://localhost/attendanceapp/login.php";
+
+    // replace with your server address
+//    private static final String API_URL = "https://10.10.31.72/attendanceapp/login.php";
+    private static final String API_URL = "http://192.168.137.1/attendanceapp/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-        requestQueue = Volley.newRequestQueue(this);
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,105 +56,47 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void loginUser(String username, String password) {
-/*        JSONObject jsonBody = new JSONObject();
-        try {
-            jsonBody.put("email", username);
-            jsonBody.put("password", password);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }*/
-
-/*
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, API_URL, jsonBody,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            boolean success = response.getBoolean("success");
-
-                            if (success) {
-                                // Login successful, navigate to the Dashboard activity
-                                Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(MainActivity.this, Dashboard.class);
-                                startActivity(intent);
-                                finish(); // Close the login activity
-                            } else {
-                                // Login failed, display an error message
-                                Toast.makeText(MainActivity.this, "username or password is invalid!", Toast.LENGTH_SHORT).show();
-                            }
-                        } catch (JSONException e) {
-                           // Log.d("eta pugyo",e.getLocalizedMessage());
-                            e.printStackTrace();
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                   //     Log.d("statusCode", String.valueOf(error.networkResponse.statusCode));
-                     //   Log.d("statusCode", error.networkResponse.data.toString());
-                        Toast.makeText(MainActivity.this, "Error occurred: " + error.networkResponse, Toast.LENGTH_SHORT).show();
-                    }
-                });*/
-
-        //     requestQueue.add(jsonObjectRequest);
-
-
+    private void loginUser(final String email, final String password) {
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
         StringRequest stringRequest = new StringRequest(Request.Method.POST, API_URL,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-
-                        if (success){
-                            Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                String message = jsonResponse.getString("message");
+                                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
                                 Intent intent = new Intent(MainActivity.this, Dashboard.class);
                                 startActivity(intent);
                                 finish();
-                        }
-                        else {
-                                 //Login failed, display an error message
+                            } else {
                                 Toast.makeText(MainActivity.this, "Username or password is invalid!", Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(MainActivity.this, "Error occurred: Invalid JSON response", Toast.LENGTH_SHORT).show();
                         }
-                        // Handle the response from the server
-//                        try {
-//                            JSONObject jsonResponse = new JSONObject(response);
-//                            boolean success = jsonResponse.getBoolean("success");
-//
-//                            if (success) {
-//                                // Login successful, navigate to the Dashboard activity
-//                                Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
-//                                Intent intent = new Intent(MainActivity.this, Dashboard.class);
-//                                startActivity(intent);
-//                                finish(); // Close the login activity
-//                            } else {
-//                                // Login failed, display an error message
-//                                Toast.makeText(MainActivity.this, "Username or password is invalid!", Toast.LENGTH_SHORT).show();
-//                            }
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // Handle the error response
-                        Toast.makeText(MainActivity.this, "Error occurred: " + error.networkResponse, Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Error occurred: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.d("TEST", error.getMessage());
                     }
                 }) {
 
             @Override
             protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("email", username);
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
                 params.put("password", password);
                 return params;
             }
         };
-        RequestQueue queue = Volley.newRequestQueue(this);
-        queue.add(stringRequest);
+        requestQueue.add(stringRequest);
     }
+
 }
